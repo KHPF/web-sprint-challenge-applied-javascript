56aa63f08edb64d6c5f5d8cb9afa5a64f69b5a25
Your Name <you@example.com>
2020-10-11 04:45:19 -0500

    sprint done, one error i couldnt figure out

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+
 // STEP 1: Create a Header component.
 // -----------------------
 // Write a function that takes no arguments and returns the markup you see below:
@@ -11,4 +12,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const div = document.createElement('div');
+    div.classList.add('header');
+
+    const span = document.createElement('span')
+    span.classList.add('date');
+    span.innerHTML = 'MARCH 28 , 2020';
+    div.append(span);
+
+    const h1 = document.createElement('h1');
+    h1.innerHTML = 'Lambda Times';
+    div.append(h1)
+
+    const span2 = document.createElement('span')
+    span.classList.add('temp');
+    span.innerHTML = '98 deg';
+    div.append(span2);
+
+
+return div;
+
+}
+
+const headerContainer = document.querySelector('div.header-container')
+
+headerContainer.appendChild(Header()) ;
